a12274349d9f1954ff69125b98a6f6368d9f6473
mayor <mayor@LAPTOP-MAYOR>
2021-07-06 01:51:05 +0300

    Добавьте файлы проекта.

---
+++ dynamics/CheckTablePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dynamics
+{
+    /// <summary>
+    /// Логика взаимодействия для CheckTablePage.xaml
+    /// </summary>
+    public partial class CheckTablePage : Page
+    {
+        public CheckTablePage(string path)
+        {
+            InitializeComponent();
+            pathTextBlock.Text = path;
+        }
+    }
+}
---
+++ dynamics/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WinForms = System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace dynamics
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onFolderSearcherClick(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog folderDialog = new WinForms.FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = @"c:\";
+            WinForms.DialogResult result = folderDialog.ShowDialog();
+
+            if (result == WinForms.DialogResult.OK)
+            {
+                String sPath = folderDialog.SelectedPath;
+                CheckTablePage checkTablePage = new CheckTablePage(sPath);
+                this.Content = checkTablePage;
+            }
+        }
+            
+    }
+}
